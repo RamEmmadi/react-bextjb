ee3f9073a9244919b85957ccac915c06e3b5fc2d
RamEmmadi <ekkaemmadi@gmail.com>
2022-08-24 14:26:07 -0500

    1 file modified

--- src/UserForm.js
+++ src/UserForm.js
@@ -5,6 +5,7 @@ const UserForm = () => {
     userName: '',
     userEmail: '',
     userAge: '40 - 50',
+    userGender: 'male',
   });
 
   const submitHandler = (e) => {
@@ -26,19 +27,57 @@ const UserForm = () => {
     <form className="userForm" onSubmit={submitHandler}>
       <div className="row">
         <label>Name</label>
-        <input name="userName" type="text" onChange={handleFormChnge} />
+        <input
+          name="userName"
+          type="text"
+          onChange={handleFormChnge}
+          value={formData.userName}
+        />
       </div>
       <div className="row">
         <label>Email</label>
-        <input name="userEmail" type="text" onChange={handleFormChnge} />
+        <input
+          name="userEmail"
+          type="text"
+          onChange={handleFormChnge}
+          value={formData.userEmail}
+        />
       </div>
       <div className="row">
         <label>Age</label>
-        <select name="userAge" onChange={handleFormChnge}>
+        <select
+          name="userAge"
+          onChange={handleFormChnge}
+          value={formData.userAge}
+        >
           <option> 30 - 40 </option>
           <option> 40 - 50 </option>
           <option> Over 50 </option>
         </select>
+      </div>
+
+      <div className="row">
+        <label>Gender</label>
+        <span>
+          <input
+            name="userGender"
+            type="radio"
+            onChange={handleFormChnge}
+            value="male"
+            checked={formData.userGender === 'male'}
+          />
+          Male
+        </span>
+        <span>
+          <input
+            name="userGender"
+            type="radio"
+            onChange={handleFormChnge}
+            value="female"
+            checked={formData.userGender === 'female'}
+          />
+          Female
+        </span>
       </div>
 
       <div className="row">
