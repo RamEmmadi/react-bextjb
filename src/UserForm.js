60c0bc687411677a907644363ffcaa48f061ddc7
RamEmmadi <ekkaemmadi@gmail.com>
2022-08-24 14:02:24 -0500

    2 files modified

--- src/UserForm.js
+++ src/UserForm.js
@@ -1,16 +1,9 @@
 import React, { useState, useEffect } from 'react';
 
 const UserForm = () => {
-  const stylesT = {
-    row: {
-      display: 'flex',
-      flexDirection: 'row',
-    },
-  };
-
   return (
-    <form>
-      <div styles={stylesT.row}>
+    <form className="userForm">
+      <div className="row">
         <label>Name</label>
         <input name="userName" />
       </div>
