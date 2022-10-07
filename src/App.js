db14e0dbbe674039d621d1c4e2ae90ac35d733fe
RamEmmadi <91928625+RamEmmadi@users.noreply.github.com>
2022-10-06 22:51:53 -0500

    1 file modified

--- src/App.js
+++ src/App.js
@@ -3,9 +3,31 @@ import './style.css';
 import MyParent from './MyParent.js';
 
 export default function App() {
+  const [data, setData] = React.useState([]);
+
+  const url = 'https://jsonplaceholder.typicode.com/todos';
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        if (json) {
+          setData(json);
+        }
+      } catch (err) {
+        setData([]);
+        console.error(err);
+      }
+    };
+    getData();
+  }, []);
+
   return (
     <div>
-      <MyParent />
+      {/* <MyParent /> */}
+      AWAIT FETCH
+      <p>{JSON.stringify(data)}</p>
     </div>
   );
 }
